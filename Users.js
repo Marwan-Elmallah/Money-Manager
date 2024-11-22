1cfefd6e61bb71a6a4fab322391fd99aa7a42f59
Marwan Elmallah <72896243+Marwan-Elmallah@users.noreply.github.com>
2024-11-22 14:47:14 +0200

    Create Users.js

---
+++ Users.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    maxlength: 100,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    match: /.+\@.+\..+/,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+});
+
+module.exports = mongoose.model('User', userSchema);
